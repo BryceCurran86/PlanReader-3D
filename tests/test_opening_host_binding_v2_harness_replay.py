13bd4a7c7632e3cb78a4471f79f577072ed7fb87
Bryce Curran <brycecurran@hotmail.com>
2026-09-16 21:30:49 +1000

    Replay normalized host validator blob

--- tests/test_opening_host_binding_v2_harness_replay.py
+++ tests/test_opening_host_binding_v2_harness_replay.py
@@ -6,9 +6,9 @@ from tools.validator_replay_harness import LaneType, ReplayConfig, run_replay
 
 BASE_SHA = "36a1f49ad92f553102101f8a2bf1d01ee45b2f52"
 VALIDATOR_REF = "gpt2/opening-host-binding-post-completeness-v2"
-VALIDATOR_COMMIT = "bb89a264b1bdd0b945c05814370d23656109ea51"
+VALIDATOR_COMMIT = "4231222348c9d575232097b9c4072ac3a83cfcba"
 VALIDATOR_PATH = "tests/test_opening_host_binding_authority_redteam_v2.py"
-VALIDATOR_BLOB = "55b2b3e87bc5263aa1693dce672c0d26aa94636b"
+VALIDATOR_BLOB = "d985d552cb67998d470570353dc0a35461266221"
 
 
 def test_frozen_host_validator_is_clean_behavioral_red_on_current_main() -> None:
